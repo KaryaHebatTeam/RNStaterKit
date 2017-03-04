28e91ee93f474eea7abceb43d1cfdb7a6b7212df
Garry Laly <garrylaly@gmail.com>
2017-03-04 11:18:27 +0700

    update button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,26 +3,37 @@ import {
   StyleSheet,
   View,
   Text,
-  TouchableHighlight
+  TouchableWithoutFeedback
 } from 'react-native';
 
 export default class Button extends Component {
   render({ onPress } = this.props) {
     return (
-      <TouchableHighlight onPress={onPress}>
+      <TouchableWithoutFeedback onPress={onPress}>
         <View style={styles.button}>
           <Text style={styles.text}>{this.props.text.toUpperCase()}</Text>
         </View>
-      </TouchableHighlight>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
 const styles = StyleSheet.create({
   button: {
-
+    backgroundColor: '#2980b9',
+    borderRadius: 5,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowRadius: 5,
+    shadowOpacity: 1,
+    shadowColor: 'black',
+    paddingHorizontal: 30,
+    paddingTop: 10,
+    paddingBottom: 12,
   },
   text: {
-
+    color: '#FFFFFF',
   }
 });
